f194142b125b97d10e9103dba09c93df569a479b
Jin the computer master <carpedm96@khu.ac.kr>
2019-04-12 10:20:09 +0900

    First commit

---
+++ gugudan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int input;
+cout << "Enter an integer";
+cin >> input;
+for ( int i = 1 ; i <= 8; ++i){ // 8단까지 만들었습니다
+cout << n << "*" << i << " = " << n*i << endl;
+}
+return 0;
+
+}
