d40b101be594b08276e4c18939bad5ac3d87b275
minhye-lee <dlalsgp001@naver.com>
2019-04-12 13:57:08 +0900

    mod gugudan.cpp

--- gugudan.cpp
+++ gugudan.cpp
@@ -5,7 +5,7 @@ int main(){
 int input;
 cout << "Enter an integer";
 cin >> input;
-for ( int i = 1 ; i <= 8; ++i){ // 8단까지 만들었습니다
+for ( int i = 1 ; i <= 9; ++i){ 
 cout << n << "*" << i << " = " << n*i << endl;
 }
 return 0;
